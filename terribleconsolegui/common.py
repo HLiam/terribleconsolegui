abe91c94c59c22315ffc5ceeade149df654a001e
HLiam <34431089+HLiam@users.noreply.github.com>
2018-03-25 23:36:49 -0600

    Optomize print_pos

--- terribleconsolegui/common.py
+++ terribleconsolegui/common.py
@@ -1,9 +1,13 @@
 from colorama import Fore, Back
 
 
+FRESET = Fore.RESET
+BRESET = Back.RESET
+
+
 def print_pos(text: str, x: int, y: int, fore=Fore.RESET, back=Back.RESET):
     """Print a colored string at a specific x and y of the console."""
-    if fore == Fore.RESET and back == Back.RESET:
+    if fore == FRESET and back == BRESET:
         print(f'\033[{y};{x}H', end=str(text))
     else:
-        print(f'\033[{y};{x}H', end=''.join((fore, back, str(text), Fore.RESET, Back.RESET)))
+        print(f'\033[{y};{x}H', end=''.join((fore, back, str(text), FRESET, BRESET)))
